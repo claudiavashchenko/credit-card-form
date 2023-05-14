d3847e5274a89604a21180851000bf793fa55312
Claudia Vashchenko <78990312+claudiavashchenko@users.noreply.github.com>
2023-05-13 21:14:40 -0500

    Added structured inputs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
 
 
 const App = () => {
+
+  const handleSubmit = () => {
+    console.log("submitted")
+  }
+
   return (
-    <div className="form-container">Claudia</div>
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+
+        <div className="input-container">
+        <label>Name on card
+          <input placeholder="name" required />
+        </label>
+        </div>
+
+        <div className="input-container">
+        <label>Card number
+          <input placeholder="0000 0000 0000 0000" minLength={16} maxLength={16} required />
+        </label>
+        </div>
+
+        <div className="supporting-inputs-container">
+          <label>Expiration date
+            <input type="month" required />
+          </label>
+          <label>CVV
+            <input placeholder="123" minLength={3} maxLength={3} required />
+          </label>
+          
+          
+        </div>
+         
+        <div className="input-container">
+          <input type="submit" />
+        </div>
+
+      </form>
+    </div>
   );
 }
 
